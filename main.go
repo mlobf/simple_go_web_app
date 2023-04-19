f08877c47da3a912a556b862a116deab9df64486
mlobf <marcoslemeborbafilho@gmail.com>
2023-04-18 21:19:30 -0300

    basic web app

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"web-app/rps"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html")
+}
+
+func playRound(w http.ResponseWriter, r *http.Request) {
+	winner, computerChoice, roundResult := rps.PlayRound(1)
+	log.Println(winner, computerChoice, roundResult)
+}
+
+func main() {
+
+	http.HandleFunc("/play", playRound)
+	http.HandleFunc("/", homePage)
+
+	log.Println("Starting at port :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string) {
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+}
